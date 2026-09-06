5285392a16ce8d2f24b095f9003593fcf39026bb
yassine <yassineaitsi4444@gmail.com>
2026-09-06 21:36:41 +0100

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -111,6 +111,15 @@ def enroll(request, course_id):
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
 #def submit(request, course_id):
+    def submit(request, course_id):
+        course = get_object_or_404(Course, pk=course_id)
+        user = request.user
+        enrollment = Enrollment.objects.get(user=user, course=course)
+        submission = Submission.objects.create(enrollment=enrollment)
+        choices = extract_answers(request)
+        submission.choices.set(choices)
+        submission_id = submission.id
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id,)))
 
 
 # An example method to collect the selected choices from the exam form from the request object
